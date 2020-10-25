8b65b2a651c06fbf3943733b0b3a0918dae58d8a
JuditBarabas <62601033+JuditBarabas@users.noreply.github.com>
2020-10-25 21:08:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function capitalize (str) {
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+console.log('Test:');
+console.log(capitalize('eggplant'));
+console.log(capitalize('pamplemousse'));
+console.log(capitalize('squid'));
